c0a3f0b9ef2a2fcf29e0f8df8b36050976d68e2d
Gabriel Feceoru <gabi.feceoru@gmail.com>
2026-01-06 20:46:26 +0100

    Extract URL path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let request_str = String::from_utf8_lossy(&buffer);
+    let lines: Vec<&str> = request_str.split("\r\n").collect();
+
+    if let Some(request_line) = lines.get(0) {
+        let parts: Vec<&str> = request_line.split_whitespace().collect();
+        let _method = parts[0];
+        let path = parts[1];
+
+        // Routing logic
+        match path {
+            "/" => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap(),
+            _ => stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap(),
+        }
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,9 +31,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                 println!("accepted new connection");
-                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+            Ok(stream) => {
+                println!("accepted new connection");
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
